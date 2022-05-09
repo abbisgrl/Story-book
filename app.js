3dbd2240fe44a1ec3b3e7a950a5afbaacd0ae3ba
Gyanendra <shahgyanendra123.gmail.com>
2022-05-09 11:35:47 +0530

    till delete request

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
-const express =require('express');
+const express = require('express');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
 const morgan = require('morgan');
 const express_handlebars = require('express-handlebars');
+const methodOverride = require('method-override')
 const path = require('path');
 const session = require('express-session');
 const passport = require('passport');
@@ -11,7 +12,7 @@ const passport = require('passport');
 
 
 //load config variable
-dotenv.config({path: './config/config.env'});
+dotenv.config({ path: './config/config.env' });
 
 
 // calling the function in app for connecting it to the database
@@ -23,20 +24,40 @@ require('./config/passport')(passport);
 
 const app = express();
 
+//body parser for post request and input body content
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json());
+
+// method override 
+app.use(methodOverride(function (req, res) {
+    if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+      // look in urlencoded POST bodies and delete it
+      let method = req.body._method
+      delete req.body._method
+      return method
+    }
+  }))
+
 //logging 
-if(process.env.NODE_ENV==='development'){
+if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'))
 }
-
+//handlebars helpers for fotmating the date and time of story
+const { formatDate, stripTags, truncate ,editIcon,select} = require('./helpers/hbs')
 
 //view engine and layout
-app.engine('.hbs', express_handlebars.engine({defaultLayout: 'main',extname: '.hbs'}));
+app.engine('.hbs', express_handlebars.engine({
+    helpers: {
+        formatDate, stripTags, truncate,editIcon,select,
+    },
+    defaultLayout: 'main', extname: '.hbs'
+}));
 app.set('view engine', '.hbs');
 app.set('views', './views');
 
 
 // static folder
-app.use(express.static(path.join(__dirname,'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 
 // session for create cookie for login
@@ -45,22 +66,28 @@ app.use(session({
     resave: false,
     saveUninitialized: false,
     // cookie: { secure: true } this not work without http
-  }))
+}))
 
 // passport middleware
 app.use(passport.initialize());
 app.use(passport.session());
 
+//set global var
+app.use(function(req,res,next){
+    res.locals.user =req.user ||null
+    next()
+})
+
 
 //redirecting to the routes
-app.use('/',require('./routes/index'));
-app.use('/auth',require('./routes/auth'))
-app.use('/stories',require('./routes/stories'));
+app.use('/', require('./routes/index'));
+app.use('/auth', require('./routes/auth'))
+app.use('/stories', require('./routes/stories'));
 
 const PORT = process.env.PORT || 3040;
 
-app.listen(PORT, function(err){
-    if (err){
+app.listen(PORT, function (err) {
+    if (err) {
         console.log(`Error in running the server: ${err}`);
     }
 
--- routes/stories.js
+++ routes/stories.js
@@ -1,12 +1,105 @@
 const express = require('express');
 const router = express.Router();
-const {ensureAuth} = require('../middleware/auth');
+const { ensureAuth } = require('../middleware/auth');
 const Story = require('../models/Story')
 
 //desc of the routes
 //url is stories/add
-router.get('/add',ensureAuth, (req,res)=>{
+router.get('/add', ensureAuth, (req, res) => {
     res.render('stories/add')
 })
 
-module.exports =router;
+router.post('/', ensureAuth, async (req, res) => {
+    try {
+        req.body.user = req.user.id
+        await Story.create(req.body)
+        res.redirect('/dashboard')
+    } catch (err) {
+        console.log(err)
+        res.render('error/500')
+    }
+})
+
+router.get('/', ensureAuth, async (req, res) => {
+    try {
+        const stories = await Story.find({ status: 'public' })
+            .populate('user')
+            .sort({ createdAt: 'desc' })
+            .lean()
+        res.render('stories/index', {
+            stories,
+        })
+    } catch (err) {
+        console.error(err)
+        res.render('error/500')
+    }
+})
+
+//desc  Show edit page
+//route Get /stories/edit/:id
+router.get('/edit/:id', ensureAuth, async (req, res) => {
+    try {
+        const story = await Story.findOne({
+            _id: req.params.id
+        }).lean()
+
+        if (!story) {
+            return res.render('error/404')
+        }
+        if (story.user != req.user.id) {
+            res.redirect('/stories')
+        } else {
+            res.render('stories/edit', { //giving story as json object so that we render title and story body
+                story,
+            })
+        }
+    } catch (error) {
+        console.error(err)
+        return res.render('error/500')
+    }
+
+})
+
+
+//desc of the routes for update story
+//url is stories/:id
+router.put('/:id', ensureAuth, async (req, res) => {
+    try {
+        let story = await Story.findById(req.params.id).lean()
+
+        if (!story) {
+            return res.render('error/404')
+        }
+
+        if (story.user != req.user.id) {
+            res.redirect('/stories')
+        } else {
+            story = await Story.findOneAndUpdate({ _id: req.params.id }, req.body, {
+                new: true,
+                runValidators: true
+            })
+            res.redirect('/dashboard')
+        }
+    } catch (err) {
+        console.error(err)
+        return res.render('error/500')
+    }
+
+
+
+})
+
+
+
+//desc of the routes for delete story
+//url is stories/:id
+router.delete('/:id', ensureAuth, async (req, res) => {
+    try {
+        await Story.remove({ _id: req.params.id })
+        res.redirect('/dashboard')
+    } catch (err) {
+        console.error(err)
+        return res.render('error/500')
+    }
+})
+module.exports = router;
